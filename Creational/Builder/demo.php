367bfb9223cca9479b5ed1a21bc78d1dee33c17e
yopaz-ninhph <ninhph@yopaz.vn>
2024-08-22 11:50:44 +0700

    feat: buider pattern

---
+++ Creational/Builder/demo.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Tạo interface để định nghĩa (thiết kế) cho builder
+ * Builder
+*/
+interface SQLQueryBuilder
+{
+    public function select(string $table, array $fields): SQLQueryBuilder;
+
+    public function where(string $field, string $value, string $operator = '='): SQLQueryBuilder;
+
+    public function limit(int $start, int $offset): SQLQueryBuilder;
+
+    // các câu truy vẫn khác ...
+
+    public function generateSQL(): string;
+}
+
+/**
+ * MysqlQueryBuilder để thao tác với mysql 
+ * ConcreteBuilder
+ */
+class MysqlQueryBuilder implements SQLQueryBuilder
+{
+    protected $query;
+
+    protected function reset(): void
+    {
+        $this->query = new \stdClass();
+    }
+
+    /**
+     * Build a base SELECT query.
+     */
+    public function select(string $table, array $fields): SQLQueryBuilder
+    {
+        $this->reset();
+        $this->query->base = "SELECT " . implode(", ", $fields) . " FROM " . $table;
+        $this->query->type = 'select';
+
+        return $this;
+    }
+
+    /**
+     * Add a WHERE condition.
+     */
+    public function where(string $field, string $value, string $operator = '='): SQLQueryBuilder
+    {
+        if (!in_array($this->query->type, ['select', 'update', 'delete'])) {
+            throw new \Exception("WHERE can only be added to SELECT, UPDATE OR DELETE");
+        }
+        $this->query->where[] = "$field $operator '$value'";
+
+        return $this;
+    }
+
+    /**
+     * Add a LIMIT constraint.
+     */
+    public function limit(int $start, int $offset): SQLQueryBuilder
+    {
+        if (!in_array($this->query->type, ['select'])) {
+            throw new \Exception("LIMIT can only be added to SELECT");
+        }
+        $this->query->limit = " LIMIT " . $start . ", " . $offset;
+
+        return $this;
+    }
+
+    /**
+     * Get the final query string.
+     */
+    public function generateSQL(): string
+    {
+        $query = $this->query;
+        $sql = $query->base;
+        if (!empty($query->where)) {
+            $sql .= " WHERE " . implode(' AND ', $query->where);
+        }
+        if (isset($query->limit)) {
+            $sql .= $query->limit;
+        }
+        $sql .= ";";
+        return $sql;
+    }
+}
+
+/**
+ * PostgresQueryBuilder để thao tác với postgres
+ * Kế thừa và ghi đè các câu truy vấn có cấu trúc khác nhau từ MysqlQueryBuilder (tính kế thừa và đa hình)
+ * ConcreteBuilder
+ */
+class PostgresQueryBuilder extends MysqlQueryBuilder
+{
+    /**
+     * override limit
+     */
+    public function limit(int $start, int $offset): SQLQueryBuilder
+    {
+        parent::limit($start, $offset);
+
+        $this->query->limit = " LIMIT " . $start . " OFFSET " . $offset;
+
+        return $this;
+    }
+
+    // các hàm khác ...
+}
+
+// Client Code
+function clientCode(SQLQueryBuilder $queryBuilder)
+{
+    $query = $queryBuilder
+        ->select("users", ["name", "email", "password"])
+        ->where("age", 18, ">")
+        ->where("age", 30, "<")
+        ->limit(10, 20)
+        ->generateSQL();
+
+    echo $query;
+}
+
+
+echo "Testing MySQL query builder:<br>";
+clientCode(new MysqlQueryBuilder());
+
+echo "<br><br><hr>";
+
+echo "Testing PostgresSQL query builder:<br>";
+clientCode(new PostgresQueryBuilder());
---
+++ Creational/Builder/note.md
@@ -0,0 +1,17 @@
+## Builder Pattern
+
+1. **Mục đích**
+- Giúp tạo ra đối phức tạp một cách dễ dàng hơn, dễ quản lý và mở rộng hơn. Việc tạo đối tượng sẽ chia thành các bước nhỏ để dễ dàng kiểm soát
+- Giảm sự phụ thuộc vào hàm constructor. Vì đã chia thành nhiều bước nhỏ nên sẽ dể hiểu hơn
+- Có tính linh hoạt cao, dễ dàng mở rộng
+2. **Nên sử dụng khi nào**
+- Cần tạo 1 đối tượng phức tạp và có nhiều tham số và cần thám số có thể tùy chỉnh
+- Việc tạo đối tượng cần nhiều bước
+- Muốn kiểm soát việc tạo đối tượng 1 cách dễ dàng
+- Việc tạo đối tượng có quá nhiều tham số trong constructor và muốn giảm phụ thuộc vào nó
+3. **Cấu trúc**
+- Builder: thường là abtract class hoặc interface để định nghĩa các phương thức của class
+- ConcreteBuilder: Là các lớp triển khai của builder. Mỗi class này sẽ biểu diễn cụ thể đối tượng
+- Client: là phần sử dụng builder để tạo ra 1 đối tượng cụ thể.
+
+Tham khảo: https://refactoring.guru/design-patterns/builder
